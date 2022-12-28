0652abd48fd496868e9a317aa9eeaab5c339b84e
Ian Camu <iancamu01@hotmail.com>
2022-12-28 18:17:24 -0300

    Ruta para editar habitaciones /rooms/:id y para activar/desactivar habitaciones con rooms/toggle/:id

--- api/src/routes/rooms.ts
+++ api/src/routes/rooms.ts
@@ -1,26 +1,53 @@
 import { Router, Request, Response } from "express";
-const {Reservation}= require("../database")
+const { Reservation } = require("../database");
 const route = Router();
 const { Room } = require("../database");
 
 route.get("/", async (_req: Request, res: Response) => {
-    const rooms = await Room.findAll({include: Reservation})
-    if(rooms){
-      res.status(200).send(rooms)
-    }else {
-      res.status(400).send("no se encontro nada")
-    }
+  const rooms = await Room.findAll({ include: Reservation });
+  if (rooms) {
+    res.status(200).send(rooms);
+  } else {
+    res.status(400).send("no se encontro nada");
+  }
+});
+
+route.put("/:id", async (req: Request, res: Response) => {
+  try {
+    await Room.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).send("Habitacion editada correctamente");
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("No se ha podido editar la habitacion");
+  }
+});
+
+route.put("/toggle/:id", async (req: Request, res: Response) => {
+  // Endpoint para activar y desactivar una habitacion, con el solo hecho de llamarla; si la habitacion esta activada la desactiva y viceversa
+  try {
+    const room = await Room.findByPk(req.params.id);
+    await Room.update(
+      { enabled: !room.enabled },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).send("Habitacion editada correctamente");
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("No se ha podido editar la habitacion");
+  }
 });
 
 route.post("/", async (req: Request, res: Response) => {
   try {
-    const { name, description, images, price } = req.body;
-    const room = await Room.create({
-      name,
-      description,
-      images,
-      price,
-    });
+    const room = await Room.create(req.body);
     res.status(200).send(room);
   } catch (error) {
     console.error(error);
