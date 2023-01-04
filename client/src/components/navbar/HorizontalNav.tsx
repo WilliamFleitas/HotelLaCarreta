c82d36a061a1ecb6589b4a86d19228e4c2ac438c
SebasUNLu <SebasUNLu@gmail.com>
2023-01-04 15:06:28 -0300

    styles: navbar

--- client/src/components/navbar/HorizontalNav.tsx
+++ client/src/components/navbar/HorizontalNav.tsx
@@ -1,28 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyle = 'p-2 hover:bg-[#E2725B] hover:text-white cursor-pointer rounded duration-500';
+
 const HorizontalNav = () => {
   return (
-    <ul className="hidden md:flex md:items-center w-9/12">
-      <li>Idioma</li>
-      <li>Tema Oscuro</li>
+    <ul className="hidden md:flex md:items-center w-9/12 justify-around">
+      <li className={linkStyle}>Idioma</li>
       <li>
-        <NavLink to="/habitaciones" className=" ">
+        <NavLink to="/habitaciones" className={linkStyle}>
           Habitaciones
         </NavLink>
       </li>
       <li>
-        <NavLink to="/sobreNosotros" className="">
+        <NavLink to="/sobreNosotros" className={linkStyle}>
           Sobre Nosotros
         </NavLink>
       </li>
       <li>
-        <NavLink to="/condiciones" className=" ">
+        <NavLink to="/condiciones" className={linkStyle}>
           Condiciones
         </NavLink>
       </li>
       <li>
-        <NavLink to="/contacto" className="">
+        <NavLink to="/contacto" className={linkStyle}>
           Contacto
         </NavLink>
       </li>
