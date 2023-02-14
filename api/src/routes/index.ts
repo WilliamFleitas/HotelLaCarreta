776db61fcfb9b5814d14bc5750c97d7e8bde56a4
William Fleitas <69325009+WilliamFleitas@users.noreply.github.com>
2023-02-14 16:26:24 -0300

    Delete api directory

--- api/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-const routes = Router();
-import rooms from "./rooms";
-import reservations from "./reservations";
-
-routes.use("/rooms", rooms);
-routes.use("/reservations", reservations);
-
-export default routes;
